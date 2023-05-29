030c2a5bf05f107469b91ed09534e4cf3e0f360f
Dilavar-Afreed <dilavar4598@gmail.com>
2023-05-29 15:19:44 +0530

    GREEN : test_masked_word_no_guess passed

---
+++ hangman.py
@@ -0,0 +1,16 @@
+import random
+
+
+def get_random_word(wordfile = "/usr/share/dict/words"):
+    candidate_words = []
+    with open(wordfile) as f:
+        for word in f:
+            word = word.strip()
+            if len(word) >= 6 and word.islower() and word.isalpha():
+                candidate_words.append(word)
+    word = random.choice(candidate_words)
+    return word
+
+def mask_word(a):
+    x="-"*len(a)
+    return x
